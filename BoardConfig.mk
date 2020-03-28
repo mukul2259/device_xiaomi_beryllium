669cf69f611a61b8bf3488835015ac254a18780a
Mukul <dhirmukul3@gmail.com>
2020-03-28 16:26:09 +0000

    beryllium: BoardConfig: Switch to Stealth

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ BOARD_HAVE_QCOM_FM := true
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG := beryllium_defconfig
+TARGET_KERNEL_CONFIG := stealth_defconfig
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
