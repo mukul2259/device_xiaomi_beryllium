dba345e4708853ee470f0c1c799823f426f2a0c2
PainKiller3 <ninadpatil100@gmail.com>
2021-05-03 23:19:53 +0530

    beryllium: Update Redfin Fingerprint To May Security Patch
    Change-Id: I214f34472d1a75676903f9925f34a5f0cbee78ab
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- wave_beryllium.mk
+++ wave_beryllium.mk
@@ -19,7 +19,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
