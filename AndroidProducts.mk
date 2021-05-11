08de0b4fe6a6121b84a0b106ab3dc39b884a2533
Mukul <dhirmukul3@gmail.com>
2021-05-11 13:05:16 +0200

    beryllium: WaveOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/wave_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    wave_beryllium-user \
+    wave_beryllium-userdebug \
+    wave_beryllium-eng
--- device.mk
+++ device.mk
@@ -10,8 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2246
-TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # FM
 PRODUCT_PACKAGES += \
@@ -22,7 +21,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
